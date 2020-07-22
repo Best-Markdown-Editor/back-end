753649647499322b8c89370e33de186922d77b57
JimmyMcBride <mcbride967@gmail.com>
2020-07-22 00:34:10 -0500

    updated

--- src/resolvers/Query/index.ts
+++ src/resolvers/Query/index.ts
@@ -10,31 +10,6 @@ const hello = async (_: void, { name }: HelloArgs) =>
 
 const users = async () => await db("users");
 
-interface UserId {
-  userId: string;
-}
-
-const getFiles = async (_: void, { userId }: UserId) => {
-  const files = await db("files").where({ userId });
-  return files.reverse();
-};
-
-interface GetFile {
-  id: number | string;
-}
-
-const getFile = async (_: void, { id }: GetFile) => {
-  const file = await db("files").where({ id }).first();
-  return file;
-};
-
-const getFileBySlug = async (_: void, { data }: any) => {
-  const file = await db("files")
-    .where({ slug: data.slug, userId: data.userId })
-    .first();
-  return file;
-};
-
 interface MyId {
   id: string;
 }
@@ -47,6 +22,62 @@ const user = async (_: void, { id }: MyId) => {
   return user;
 };
 
+interface UserId {
+  userId: string;
+}
+
+const getFiles = async (_: void, { userId }: UserId) => {
+  const files = await db("files")
+    .where({ userId })
+    .orderBy("updatedAt", "desc");
+  return files;
+};
+
+interface GetFile {
+  id: number | string;
+}
+
+const getFile = async (_: void, { id }: GetFile) => {
+  const file = await db("files").where({ id }).first();
+  return file;
+};
+
+interface GetFileBySlugArgs {
+  data: {
+    slug: string;
+    userId: string;
+  };
+}
+
+const getFileBySlug = async (_: void, { data }: GetFileBySlugArgs) => {
+  const file = await db("files")
+    .where({ slug: data.slug, userId: data.userId })
+    .first();
+  return file;
+};
+
+const isSubscribed = async (_: void, { id }: MyId) => {
+  const user = await db("users").where({ id }).first();
+
+  return user.subscriber;
+};
+
+const getFolder = async (_: void, { id }: MyId) => {
+  const folder = await db("folder").where({ id }).first();
+
+  if (!folder) throw new Error("No folder with this ID 💀");
+
+  return folder;
+};
+
+const getFolders = async (_: void, { userId }: UserId) => {
+  const folders = await db("folder").where({ userId });
+
+  if (!folders) throw new Error("No folders belong to  💀");
+
+  return folders;
+};
+
 const Query = {
   hello,
   users,
@@ -54,6 +85,9 @@ const Query = {
   getFiles,
   getFile,
   getFileBySlug,
+  isSubscribed,
+  getFolder,
+  getFolders,
 };
 
 export default Query;
