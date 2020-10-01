2a68a547e9ab8b4fd88330e226c4ada4d3d520dd
JimmyMcBride <mcbride967@gmail.com>
2020-10-01 12:29:05 -0500

    added eslint and prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {},
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
