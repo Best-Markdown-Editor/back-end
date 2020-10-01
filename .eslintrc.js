185d7f387e89202c94198e3ac337fd39005a4361
JimmyMcBride <mcbride967@gmail.com>
2020-10-01 18:18:46 -0500

    this should pass

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2020: true,
+  },
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -16,10 +18,13 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "prettier/prettier": ["error"],
+    // "@typescript-eslint/no-unused-vars": ["error"],
+    // "@typescript-eslint/no-unused-vars": ["error"],
   },
   overrides: [
     {
-      files: ["**/*.test.ts"],
+      files: ["__tests__/*", "__tests__/**/*"],
       env: {
         jest: true,
       },
